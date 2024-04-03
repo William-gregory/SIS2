7127f42fb03d7705f7a947804ebca82f1a2874c0
William-gregory <william.gregory@outlook.com>
2024-04-03 12:44:28 -0400

    remove positive increment thresh for SSTadj

--- src/SIS_G23_CNN.F90
+++ src/SIS_G23_CNN.F90
@@ -281,7 +281,7 @@ subroutine CNN_inference(IST, OSS, FIA, IOF, G, IG, CS, CNN, dt_slow)
      enddo
      IST%part_size(i,j,0) = posterior(i,j,0)
      if (CNN%do_SSTadj) then
-        if (cvr > CNN%SIC_thresh .and. sic_inc > 0.0 .and. OSS%SST_C(i,j) > OSS%T_fr_ocn(i,j)) then
+        if (cvr > CNN%SIC_thresh .and. OSS%SST_C(i,j) > OSS%T_fr_ocn(i,j)) then
            IOF%flux_sh_ocn_top(i,j) = IOF%flux_sh_ocn_top(i,j) - &
                 ((OSS%T_fr_ocn(i,j) - OSS%SST_C(i,j)) * (1035.0*3925.0) * (CNN%piston_SSTadj/86400.0)) !1035 = reference density, 3925 = Cp of water
         endif
